5c2bbd72272033b2971e4dfc3de6dd2d9a3cc377
kiyoshi katsuda <k-katsuda@mediar.co.jp>
2020-05-07 18:27:42 +0900

    タスク関数の場所を変更

--- src/controller/index.go
+++ src/controller/index.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/k-katsuda/post-checker/src/model"
 )
 
@@ -26,4 +30,21 @@ func FindByID(id uint) model.Task {
 		task.Postcount = postCount
 	}
 	return task
+}
+
+func TasksPOST(c *gin.Context) {
+	db := model.DBConnect()
+
+	postFlg := c.PostForm("postFlg")
+	postCount := c.PostForm("postCount")
+
+	_, err := db.Exec("UPDATE post SET postFlg = ?, postCount=? WHERE id = 1", postFlg, postCount)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	task := FindByID(1)
+
+	fmt.Println(task)
+	c.JSON(http.StatusOK, gin.H{"task": task})
 }
